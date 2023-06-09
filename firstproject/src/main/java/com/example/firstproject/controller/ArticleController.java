203bbb17a7efd6a4dfaea4ec54cf574dc5f1fc6b
change-challenge <dog941128@hotmail.com>
2023-06-10 00:03:40 +0900

    Feat: Read Articles - 개별 검색 & 전체 검색

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,12 +3,21 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
 @Controller
+@Slf4j // 로깅을 위한 어노테이션
 public class ArticleController {
 
         @Autowired // 스프링 부트가 미리 생성해놓은 객체를 가져다가 자동 연결!
@@ -20,15 +29,47 @@ public class ArticleController {
         }
         @PostMapping("/articles/create")
         public String createArticle(ArticleForm form) {
-            System.out.println(form.toString());
+//            System.out.println(form.toString()); -> 서버 성능 저하, 로깅기능으로 대체!
+            log.info(form.toString());
 
             // 1. Dto를 변환! Entity
             Article article = form.toEntity();
-            System.out.println(article.toString());
+//            System.out.println(article.toString());
+            log.info(article.toString());
 
             // 2. Repository에게 Entity를 DB안에 저장하게 함!
             Article saved = articleRepository.save(article);
-            System.out.println(saved.toString());
+//            System.out.println(saved.toString());
+            log.info(saved.toString());
             return "";
+        }
+
+        @GetMapping("/articles/{id}")
+        public String show(@PathVariable Long id, Model model) {
+            log.info("id = " + id);
+
+            // 1. id로 데이터를 가져옴!
+            Article articleEntity = articleRepository.findById(id).orElse(null);
+
+            // 2. 가져온 데이터를 모델로 등록!
+            model.addAttribute("article", articleEntity);
+
+            // 3. 보여줄 페이지를 설정!
+            return "articles/show";
+        }
+
+        @GetMapping("/articles")
+        public String index(Model model) {
+            // 1. 모든 Article을 가져온다!
+            // findAll의 return이 Iterable이라 articleRepository를 들어가서 override했다.
+            // ArrayList로 만들어놨는데 ArrayList의 상위 타입인 List로 upcasting했다.
+            List<Article> articleEntityList = articleRepository.findAll();
+//            Iterable<Article> articleEntityList = articleRepository.findAll();
+
+            // 2. 가져온 Article 묶음을 View로 전달!
+            model.addAttribute("articleList", articleEntityList);
+
+            // 3. View 페이지를 설정!
+            return "articles/index"; // articles/index.mustache
         }
 }
