6d314aa3c987fe3b61dd46611b28ac9f4d4e3c2b
change-challenge <dog941128@hotmail.com>
2023-06-27 12:20:06 +0900

    feat: common Components LabelInput 추가

---
+++ [front]Hyundai_book_react/src/components/Common/LabelInput.jsx
@@ -0,0 +1,60 @@
+import React, { forwardRef } from 'react'
+import styled from 'styled-components'
+
+const InputWrap = styled.div`
+  display: flex;
+  border-radius: 8px;
+  padding: 16px;
+  margin-top: 8px;
+  background-color: white;
+  border: 1px solid ${({ theme }) => theme.colors.grey4};
+  margin-top: 15px;
+
+  &:focus-within {
+    border: 1px solid ${({ theme }) => theme.colors.main};
+  }
+`
+
+const Input = styled.input`
+  width: 100%;
+  outline: none;
+  border: none;
+  height: 17px;
+  font-size: 14px;
+  font-weight: 400;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.grey2};
+  }
+`
+
+const LabelInput = forwardRef(
+  (
+    {
+      type = 'text',
+      value = '',
+      onChange,
+      className = '',
+      placeholder = 'Default',
+      ...restProps
+    },
+    ref
+  ) => {
+    return (
+      <InputWrap className={className}>
+        <Input
+          type={type}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          {...restProps}
+          ref={ref}
+        />
+      </InputWrap>
+    )
+  }
+)
+
+LabelInput.displayName = 'LabelInput'
+
+export default LabelInput
